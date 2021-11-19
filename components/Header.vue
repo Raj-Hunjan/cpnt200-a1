ba3282854c1bdc5a988ea9c3c301e4bb542ccaab
Raj-Hunjan <rajhunjann@gmail.com>
2021-11-19 14:04:05 -0700

    changed some styling

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,5 @@
 <template>
-    <header class="grid justify-items-center">
-        <h1>hello world</h1>
-        <p>hello!</p>
+    <header class="grid justify-items-center pt-9 font-bold-5">
+        <h1>Some Website</h1>
     </header>
 </template>
