767b79acb4e1404158f0aeff7aecec879fcac744
Raj-Hunjan <rajhunjann@gmail.com>
2021-11-20 20:54:13 -0700

    added files, a image and fixed image issue

---
+++ components/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+  <section class="grid justify-items-center bg-purple-900 text-white p-7">
+    <p>&copy; Raj</p>
+  </section>
+</template>
---
+++ components/Gallery.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="grid justify-items-center p-14 text-5xl">
+    <h2>Gallery</h2>
+    <p class="p-14">
+      Some images of cars
+    </p>
+
+    <div class="flex justify-evenly gap-x-16 gap-y-16 flex-wrap p-14">
+      <nuxt-img
+        src="images/image1.jpg"
+        sizes="sm:300px md:400px lg:500px"
+      />
+      <nuxt-img
+        src="images/image2.jpg"
+        sizes="sm:300px md:400px lg:500px"
+      />
+      <nuxt-img
+        src="images/image3.jpg"
+        sizes="sm:300px md:400px lg:500px"
+      />
+    </div>
+  </section>
+</template>
